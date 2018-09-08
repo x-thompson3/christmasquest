1e340b8475ae8b7851a9e2238d24fa79eba4b996
Xander Thompson <x.thompson3@gmail.com>
2018-09-08 11:49:12 -0700

    Automated story_log push

--- architecture/game.py
+++ architecture/game.py
@@ -1,9 +1,19 @@
 class GameEngine:
-    """Class for holding current game state and log of the adventure"""
+    """Class for holding current game state and a log of the adventure"""
     def __init__(self, story_dir="christmas18"):
         self.phase = 1
         self.story_dir = story_dir
+        self.index = 1
+        self.answer_set = {}
 
-    def logit(self, message):
-        with open('game.log', "a") as f:
+    def get_current_story(self):
+        pass
+
+    def check_answer(self, answer):
+        pass
+
+    def logit(self, message, public=False):
+        """Log the results to the internal.log"""
+        log_file = 'The_Story_Thus_Far.log' if public else 'internal.log'
+        with open(log_file, "a") as f:
             f.write(message)
---
+++ architecture/push_log.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+cd ~/python-virtual-environments/test/christmasquest
+git add The_Story_Thus_Far.log
+git commit -m "Updating the story!"
+git push
